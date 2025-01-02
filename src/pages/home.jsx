b5d0cfc9f5490c84322b991cbebfe0a4385100a6
JoaoRSS <joaors987654321@gmail.com>
2025-01-02 17:38:48 -0300

    atualizado home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,18 +2,6 @@ import Categoria from "../components/categoria";
 import { useVideoContext } from "@/hooks/useVideoContext";
 import ModalEditar from "../components/modalEditar";
 import Destaque from "../components/destaque";
-import styled from "styled-components";
-
-const CategriaContainer = styled.div`
-    display: flex;
-    align-items: center;
-    align-self: center;
-    justify-self: center;
-    margin-top: 50px;
-    width: 100vw;
-    flex-wrap: wrap;
-    gap: 20px;
-`
 
 export default function Home() {
     const { videos } = useVideoContext();
@@ -23,11 +11,10 @@ export default function Home() {
     return(
         <>
             <Destaque />
-            <CategriaContainer>
+
                 {hasVideosWithTag("frontend") && <Categoria tag="frontend" />}
                 {hasVideosWithTag("backend") && <Categoria tag="backend" />}
                 {hasVideosWithTag("mobile") && <Categoria tag="mobile" />}
-            </CategriaContainer>
 
             <ModalEditar />
         </>
